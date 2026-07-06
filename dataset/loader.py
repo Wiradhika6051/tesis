7cee24efe51b66d4cbf9e69d8ce58217305684ba
Wiradhika6051 <anugrahdwfawwaz@gmail.com>
2026-07-06 11:58:47 +0700

    refactor: refactor loader

--- dataset/loader.py
+++ dataset/loader.py
@@ -2,36 +2,71 @@ import json
 import pickle
 
 
+def load_samples(
+    jsonl_path,
+    strategy,
+    previous_source_path=None
+):
 
-def load_samples(jsonl_path, strategy):
-    with open(
-        "dataset/previous_sources.pkl",
-        "rb"
-    ) as f:
-        previous_lookup = pickle.load(f)
+    if previous_source_path:
+
+        with open(previous_source_path, "rb") as f:
+            previous_lookup = pickle.load(f)
+
+    else:
+
+        previous_lookup = {}
 
     samples = []
 
-    with open(jsonl_path, "r", encoding="utf-8") as f:
+    with open(
+        jsonl_path,
+        "r",
+        encoding="utf-8"
+    ) as f:
 
         for line in f:
 
             data = json.loads(line)
 
-            for repo in data.values():
+            for repo_url, repo in data.items():
 
                 for commit in repo.values():
-                
-                    files = commit.get("files", {})
+
+                    files = commit.get(
+                        "files",
+                        {}
+                    )
 
                     for file in files.values():
-                        file["previousSource"] = previous_lookup.get(                    
-                            (
-                                repo,
-                                commit["sha"],
-                                file["filename"]
-                            )
+
+                        filename = file[
+                            "filename"
+                        ].lstrip("/")
+
+                        key = (
+                            repo_url,
+                            commit["sha"],
+                            filename
                         )
-                        samples.extend(strategy.extract(file,next(iter(data))))
+
+                        previous = previous_lookup.get(
+                            key
+                        )
+
+                        file["previousSource"] = (
+                            previous["source"]
+                            if previous is not None
+                            else None
+                        )
+
+                        samples.extend(
+
+                            strategy.extract(
+                                file,
+                                repo_url
+                            )
+
+                        )
 
     return samples
