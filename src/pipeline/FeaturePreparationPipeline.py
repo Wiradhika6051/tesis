6e090f919eb1e1616191087232d0e8caba57a0f6
Wiradhika6051 <anugrahdwfawwaz@gmail.com>
2026-07-27 10:33:01 +0700

    feat: add feature preparaton pipeline

---
+++ src/pipeline/FeaturePreparationPipeline.py
@@ -0,0 +1,52 @@
+class FeaturePreparationPipeline:
+
+    def __init__(
+        self,
+        encoder,
+        token_vocab_builder,
+        cfg_vocab_builder
+    ):
+        self.encoder = encoder
+        self.token_vocab_builder = token_vocab_builder
+        self.cfg_vocab_builder = cfg_vocab_builder
+
+    def prepare(
+        self,
+        samples
+    ):
+
+        #
+        # Build vocabularies.
+        #
+        token_vocab = self.token_vocab_builder(
+            samples
+        )
+
+        cfg_vocab = self.cfg_vocab_builder(
+            samples
+        )
+
+        #
+        # Update encoder with the vocabularies.
+        #
+        self.encoder.token_vocab = token_vocab
+        self.encoder.cfg_vocab = cfg_vocab
+
+        #
+        # Encode every sample.
+        #
+        encoded_samples = []
+
+        for sample in samples:
+
+            encoded_samples.append(
+                self.encoder.encode(
+                    sample
+                )
+            )
+
+        return (
+            encoded_samples,
+            token_vocab,
+            cfg_vocab
+        )
