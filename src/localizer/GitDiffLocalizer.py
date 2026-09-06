5e613cc685c094dcb10c5b3f2fb04a1c708e49a3
Wiradhika6051 <anugrahdwfawwaz@gmail.com>
2026-09-06 22:47:50 +0700

    chore: comment debug

--- src/localizer/GitDiffLocalizer.py
+++ src/localizer/GitDiffLocalizer.py
@@ -60,11 +60,11 @@ class GitDiffLocalizer(DiffLocalizer):
 
                 new_line += 1
                 continue
-            print("DIFF:")
-            # print(sample.diff)
+            # print("DIFF:")
+            # # print(sample.diff)
 
-            print("LABEL:", sample.label)
-            print("SEED LINES:", sample.seed_lines)
+            # print("LABEL:", sample.label)
+            # print("SEED LINES:", sample.seed_lines)
             #
             # Context line
             #
